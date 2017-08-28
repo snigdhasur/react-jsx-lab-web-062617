2a112d1d20aa5aa096df362a08921393a60cdcbd
Snigdha Sur <Snigdha@Snigdhas-MacBook-Air-2.local>
2017-08-28 10:07:57 -0400

    Done.

---
+++ src/components/FillerText.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class FillerText extends React.Component {
+  render() {
+    return (
+      <p>
+       I am a filler text. I can be used to fill your screen. Amazing!
+      </p>
+    )
+  }
+};
+
+export default FillerText;
+
